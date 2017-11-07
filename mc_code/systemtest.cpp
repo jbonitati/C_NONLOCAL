dcb66ab459fe655ab161dcce80a7c9f14aeceb35
Joey Bonitati <joeybonitati@gmail.com>
2017-11-07 16:30:42 -0500

    added example object to systemtest to start unit testing

--- mc_code/systemtest.cpp
+++ mc_code/systemtest.cpp
@@ -10,16 +10,48 @@
 #include "system.h"
 #include <iostream>
 
+using namespace std;
+
 // ------------- Tests Follow --------------
 //Name your test cases for what they test
 BOOST_AUTO_TEST_CASE( constructor )
 {
-  /*System system1(const double a_size, double e,
-      Particle projectile, Particle target, double rc,
-      int c0, std::vector<Channel> channels_,
-      OpticalPotential op, NonLocalOpticalPotential nlop, int basis_size,
-      double step, double max,matrix<CouplingPotential> coupling);
-  */
+  double a = 10.0;
+  double energy = 5.0;
+  Particle projectile(1,0);
+  Particle target(10,4);
+  double rc = 1.0; //coulomb radius
+  int c0 = 1; //entrance channel
+  OpticalPotential op(V_Potential(0, 0, 0), V_Potential(0, 0, 0),
+    D_Potential(0, 0, 0), D_Potential(0,0,0),
+    SO_Potential(0, 0, 0), SO_Potential(0, 0, 0));
+  NonLocalOpticalPotential nlop(V_Potential(0, 0, 0), 
+    V_Potential(0, 0, 0),
+    D_Potential(0, 0, 0), D_Potential(0,0,0),
+    SO_Potential(0, 0, 0), SO_Potential(0, 0, 0), 0);
+  int b_size = 10; //basis size
+  double step = .1;
+  double max = 15;
+  matrix<CouplingPotential> coupling;
+  std::vector<Channel> channels;
+  double mu = 1.0;//reduced mass
+  channels.push_back(Channel(
+    0,//e
+    0,//l
+    0,//m
+    0,//j
+    mu, energy, a, target, projectile));
+      
+  //One channel case
+  System system1(const double a, double energy,
+    Particle projectile, Particle target, double rc,
+    int c0, std::vector<Channel> c,
+    OpticalPotential op, NonLocalOpticalPotential nlop, int b_size,
+    double step, double max, matrix<CouplingPotential> coupling);
+  
+  //////////////////////////////////////////////////////////////////////
+  //todo: add tests here, e.g:
+  //BOOST_EQUALS(system1.Cmatrix, ((0, 1), (1, 0)));
   
   std::cout << "Testing!" << std::endl;
 }
